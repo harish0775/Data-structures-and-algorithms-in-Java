de66c22deaad1842db2ddcb1b83a52802956dedd
Harish Nishad <harish.nishad0775@gmail.com>
2020-12-29 23:28:32 +0530

    Learn Recurssion Basic

--- Practice/Hello.java
+++ Practice/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
   }
   
   public static void main(String[] args) {
-    int myNum1 = plusMethodInt(8, 23);
+    int myNum1 = plusMethodInt(8, 34);
     double myNum2 = plusMethodDouble(4.3, 6.26);
     System.out.println("int: " + myNum1);
     System.out.println("double: " + myNum2);
