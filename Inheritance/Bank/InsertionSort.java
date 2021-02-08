8551193b15031f0e1e3c02a1f6df988ea7d88037
Harish Nishad <harish.nishad0775@gmail.com>
2021-02-08 23:39:55 +0530

    add to Insertion sort

---
+++ Inheritance/Bank/InsertionSort.java
@@ -0,0 +1,36 @@
+
+// Insertion sort in Java
+
+import java.util.Arrays;
+
+class InsertionSort {
+
+  void insertionSort(int array[]) {
+    int n = array.length;
+
+    for (int i = 1; i < n; i++) {
+      int key = array[i];
+      int j = i - 1;
+
+      // Compare key with each element on the left of it until an element smaller than
+      // it is found.
+      // For descending order, change key<array[j] to key>array[j].
+      while (j >= 0 && key < array[j]) {
+        array[j + 1] = array[j];
+        j--;
+      }
+
+      // Place key at after the element just smaller than it.
+      array[j + 1] = key;
+    }
+  }
+
+  // Driver code
+  public static void main(String args[]) {
+    int[] data = { 9, 5, 1, 4, 3 };
+    InsertionSort is = new InsertionSort();
+    is.insertionSort(data);
+    System.out.println("Sorted Array in Ascending Order: ");
+    System.out.println(Arrays.toString(data));
+  }
+}
